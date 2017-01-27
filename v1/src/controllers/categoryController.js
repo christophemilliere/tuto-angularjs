2683e023a40b911e281721d3d485280d66488e92
christophe <christophemilliere93@gmail.com>
2017-01-27 17:13:23 +0100

    add controller

---
+++ v1/src/controllers/categoryController.js
@@ -0,0 +1,9 @@
+'use strict'
+app.controller('categoryIndex', function($scope){
+
+})
+.controller('categoryList', function($scope){})
+.controller('categoryCreate', function($scope){
+  $scope.test = "otot"
+})
+.controller('categoryRemove', function($scope){});
---
+++ v1/src/controllers/itemController.js
@@ -0,0 +1,9 @@
+'use strict'
+app.controller('itemIndex', function($scope){
+
+})
+.controller('itemList', function($scope){})
+.controller('itemCreate', function($scope){
+  $scope.test = "item"
+})
+.controller('itemRemove', function($scope){});
